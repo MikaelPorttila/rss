f134c9a4d46efafed9455629af1ba1b5980a397e
Mikael Porttila <mikael.porttila@gmail.com>
2020-06-24 20:51:13 +0200

    Fixed test, removed unstable API calls

--- test.ts
+++ test.ts
@@ -4,31 +4,21 @@ import {
 import {
   parseRss,
 } from "./mod.ts";
-import { walk } from "https://deno.land/std/fs/mod.ts";
 
 const decoder = new TextDecoder("utf-8");
-let cache: any = {};
-const loadSample = async (sampleName: string): Promise<string> => {
-  let cacheResult = cache[sampleName];
-  if (!cacheResult) {
-    const result = await Deno.readFile(`./samples/${sampleName}.xml`);
-    cacheResult = cache[sampleName] = decoder.decode(result);
-  }
-  return cacheResult;
-};
-
-Deno.test("Parse samples", async () => {
-  for await (const entry of walk("./samples/")) {
-    if (entry.isFile) {
-      const fileContent = await Deno.readFile(`./${entry.path}`);
+for (const fileInfo of Deno.readDirSync('./samples/')) {
+  if (fileInfo.isFile) {
+    Deno.test(`Parse ${fileInfo.name}`, async () => {
+      const filePath = `./samples/${fileInfo.name}`;
+      const fileContent = await Deno.readFile(filePath);
       const feed = decoder.decode(fileContent);
       try {
         const [feedType, result] = await parseRss(feed);
         assert(() => !!feedType);
         assert(() => !!result);
       } catch (err) {
-        throw `Failed to parse ${entry.path}`;
+        throw `Failed to parse ${fileInfo.name}`;
       }
-    }
+    });
   }
-});
+}
