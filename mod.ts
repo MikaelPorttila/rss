314569c6443e15baf0cdf6c023faafc0d4b21d5d
Mikael Porttila <mikael.porttila@gmail.com>
2021-07-14 22:28:19 +0200

    Extended mod.ts export

--- mod.ts
+++ mod.ts
@@ -11,6 +11,12 @@ export  {
 	FeedType
 } from "./src/types/mod.ts";
 
-export type { Options } from "./src/deserializer.ts";
-export { deserializeFeed } from "./src/deserializer.ts";
+export type {
+	Options
+} from "./src/deserializer.ts";
+
+export {
+	parseFeed,
+	deserializeFeed
+} from "./src/deserializer.ts";
 
