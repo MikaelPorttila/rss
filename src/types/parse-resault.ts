cf6f5fb459c49127d0d510179b97db99fc4c5c82
Mikael Porttila <mikael.porttila@gmail.com>
2021-07-20 17:02:15 +0200

    fix typo

--- src/types/parse-resault.ts
+++ src/types/parse-resault.ts
@@ -3,7 +3,7 @@ import { InternalRSS1 } from "./internal-rss1.ts";
 import { InternalRSS2 } from "./internal-rss2.ts";
 import { Atom, RSS1, RSS2 } from "./mod.ts";
 
-export interface FeedParseResault {
+export interface FeedParseResult {
 	feedType: Atom | RSS1 | RSS2;
 	data: InternalAtom | InternalRSS1 | InternalRSS2;
 }
