ba282ce1572d1ec15df48d81ae9482f46523e734
Mikael Porttila <mikael.porttila@gmail.com>
2021-07-27 00:17:13 +0200

    Resolver tests

--- src/resolvers/rss2-resolver.ts
+++ src/resolvers/rss2-resolver.ts
@@ -1,6 +1,7 @@
 import { DublinCoreFields } from "../types/dublin-core.ts";
 import { resolveDublinCoreField } from "./dublin-core-resolver.ts";
 import { resolveMediaRssField } from './media-rss-resolver.ts';
+import { Rss2Fields } from "./types/rss2-fields.ts";
 
 export const resolveRss2Field = (
   nodeName: string,
@@ -11,57 +12,54 @@ export const resolveRss2Field = (
   let propertyName = nodeName;
 
   switch (nodeName) {
-    case Field.TextInput:
+    case Rss2Fields.TextInput:
       propertyName = "textInput";
       break;
-    case Field.SkipHours:
+    case Rss2Fields.SkipHours:
       propertyName = "skipHours";
       break;
-    case Field.SkipDays:
+    case Rss2Fields.SkipDays:
       propertyName = "skipDays";
       break;
-    case Field.PubDate:
+    case Rss2Fields.PubDate:
       propertyName = "pubDate";
       isDate = true;
       break;
-    case Field.ManagingEditor:
+    case Rss2Fields.ManagingEditor:
       propertyName = "managingEditor";
       break;
-    case Field.WebMaster:
+    case Rss2Fields.WebMaster:
       propertyName = "webMaster";
       break;
-    case Field.LastBuildDate:
+    case Rss2Fields.LastBuildDate:
       propertyName = "lastBuildDate";
       isDate = true;
       break;
-    case Field.Item:
+    case Rss2Fields.Item:
       propertyName = "items";
       isArray = true;
       break;
-		case Field.Enclosure:
+		case Rss2Fields.Enclosure:
 			isArray = true;
 			break;
-    case Field.Category:
+    case Rss2Fields.Category:
       propertyName = "categories";
       isArray = true;
       break;
-    case DublinCoreFields.Creator:
-      isArray = true;
-      break;
-    case Field.isPermaLink:
+    case Rss2Fields.isPermaLink:
       propertyName = "isPermaLink";
       break;
-    case Field.Ttl:
-    case Field.Length:
-    case Field.Width:
-    case Field.Height:
+    case Rss2Fields.Ttl:
+    case Rss2Fields.Length:
+    case Rss2Fields.Width:
+    case Rss2Fields.Height:
       isNumber = true;
       break;
-    case Field.Hour:
+    case Rss2Fields.Hour:
       isArray = true;
       isNumber = true;
       break;
-    case Field.Day:
+    case Rss2Fields.Day:
       isArray = true;
       break;
     default:
@@ -94,36 +92,3 @@ export const resolveRss2Field = (
   return [propertyName, isArray, isNumber, isDate];
 };
 
-enum Field {
-  Author = "author",
-  Category = "category",
-  Channel = "channel",
-  Cloud = "cloud",
-  Copyright = "copyright",
-  Description = "description",
-  Docs = "docs",
-  Enclosure = "enclosure",
-  Generator = "generator",
-  Guid = "guid",
-  Height = "height",
-  Image = "image",
-  isPermaLink = "ispermalink",
-  Item = "item",
-  Language = "language",
-  LastBuildDate = "lastbuilddate",
-  Length = "length",
-  Link = "link",
-  ManagingEditor = "managingeditor",
-  PubDate = "pubdate",
-  SkipDays = "skipdays",
-  SkipHours = "skiphours",
-  Source = "source",
-  TextInput = "textinput",
-  Title = "title",
-  Ttl = "ttl",
-  Url = "url",
-  WebMaster = "webmaster",
-  Width = "width",
-  Hour = "hour",
-  Day = "day",
-}
--- src/resolvers/rss2-resolver_test.ts
+++ src/resolvers/rss2-resolver_test.ts
@@ -1,7 +1,189 @@
 import { assert, assertEquals } from "../../test_deps.ts";
+import { DublinCoreFields } from "../types/dublin-core.ts";
 import { resolveRss2Field } from "./rss2-resolver.ts";
+import { Rss2Fields } from "./types/rss2-fields.ts";
 
-Deno.test("RSS 2.0 resolver", () => {
+[
+  {
+    propertyName: "",
+    expect: {
+      isArray: false,
+      isNumber: false,
+      isDate: false,
+      propertyName: '',
+    }
+  },
+  {
+    propertyName: Rss2Fields.TextInput,
+    expect: {
+      isArray: false,
+      isNumber: false,
+      isDate: false,
+      propertyName: 'textInput'
+    }
+  },
+	{
+    propertyName: Rss2Fields.SkipHours,
+    expect: {
+      isArray: false,
+      isNumber: false,
+      isDate: false,
+      propertyName: 'skipHours'
+    }
+  },
+	{
+    propertyName: Rss2Fields.SkipDays,
+    expect: {
+      isArray: false,
+      isNumber: false,
+      isDate: false,
+      propertyName: 'skipDays'
+    }
+  },
+	{
+    propertyName: Rss2Fields.PubDate,
+    expect: {
+      isArray: false,
+      isNumber: false,
+      isDate: true,
+      propertyName: 'pubDate'
+    }
+  },
+	{
+    propertyName: Rss2Fields.ManagingEditor,
+    expect: {
+      isArray: false,
+      isNumber: false,
+      isDate: false,
+      propertyName: 'managingEditor'
+    }
+  },
+	{
+    propertyName: Rss2Fields.WebMaster,
+    expect: {
+      isArray: false,
+      isNumber: false,
+      isDate: false,
+      propertyName: 'webMaster'
+    }
+  },
+	{
+    propertyName: Rss2Fields.LastBuildDate,
+    expect: {
+      isArray: false,
+      isNumber: false,
+      isDate: true,
+      propertyName: 'lastBuildDate'
+    }
+  },
+	{
+    propertyName: Rss2Fields.Item,
+    expect: {
+      isArray: true,
+      isNumber: false,
+      isDate: false,
+      propertyName: 'items'
+    }
+  },
+	{
+    propertyName: Rss2Fields.Enclosure,
+    expect: {
+      isArray: true,
+      isNumber: false,
+      isDate: false,
+      propertyName: Rss2Fields.Enclosure
+    }
+  },
+	{
+    propertyName: Rss2Fields.Category,
+    expect: {
+      isArray: true,
+      isNumber: false,
+      isDate: false,
+      propertyName: 'categories'
+    }
+  },
+	{
+    propertyName: Rss2Fields.isPermaLink,
+    expect: {
+      isArray: false,
+      isNumber: false,
+      isDate: false,
+      propertyName: 'isPermaLink'
+    }
+  },
+	...[
+		Rss2Fields.Ttl,
+		Rss2Fields.Length,
+		Rss2Fields.Width,
+		Rss2Fields.Height
+	].map((field) => (
+		{
+			propertyName: field,
+			expect: {
+				isArray: false,
+				isNumber: true,
+				isDate: false,
+				propertyName: field
+			}
+		}
+	)),
+	{
+    propertyName: Rss2Fields.Hour,
+    expect: {
+      isArray: true,
+      isNumber: true,
+      isDate: false,
+      propertyName: Rss2Fields.Hour
+    }
+  },
+	{
+    propertyName: Rss2Fields.Day,
+    expect: {
+      isArray: true,
+      isNumber: false,
+      isDate: false,
+      propertyName: Rss2Fields.Day
+    }
+  },
+	{
+    propertyName: DublinCoreFields.Date,
+    expect: {
+      isArray: false,
+      isNumber: false,
+      isDate: true,
+      propertyName: DublinCoreFields.Date
+    }
+  },
+	{
+    propertyName: DublinCoreFields.Valid,
+    expect: {
+      isArray: true,
+      isNumber: false,
+      isDate: true,
+      propertyName: DublinCoreFields.Valid
+    }
+  },
+	{
+    propertyName: DublinCoreFields.Creator,
+    expect: {
+      isArray: true,
+      isNumber: false,
+      isDate: false,
+      propertyName: DublinCoreFields.Creator
+    }
+  },
+].forEach((test) => {
+  Deno.test(`resolveRss2Field:${test.propertyName}`, () => {
+    const [propertyName, isArray, isNumber, isDate] = resolveRss2Field(test.propertyName);
+    assertEquals(isArray, test.expect.isArray, "isArray");
+    assertEquals(isNumber, test.expect.isNumber, "isNumber");
+    assertEquals(propertyName, test.expect.propertyName, '');
+    assertEquals(isDate, test.expect.isDate, "isDate");
+  });
+});
+
+/* Deno.test("RSS 2.0 resolver", () => {
   [undefined, null].forEach((field: any) => {
     const [propertyName, isArray, isNumber, isDate] = resolveRss2Field(field);
 
@@ -50,3 +232,4 @@ Deno.test("RSS 2.0 resolver", () => {
     assertEquals(true, isDate, `isDate should be true for field: ${field}`);
   });
 });
+ */
